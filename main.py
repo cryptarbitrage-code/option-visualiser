3e604aa580202963f34cc41d5bb7ae6dc2001341
cryptarbitrage-code <cryptosheetsharing+github@gmail.com>
2022-03-17 17:07:11 +0000

    add chart, clean up option_calculations.py

--- main.py
+++ main.py
@@ -1,16 +1,123 @@
-# This is a sample Python script.
+from tkinter import *
+from tkinter import ttk
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+import option_calculations as oc
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Some chart parameters
+step_number = 100
+chart_size = (6, 4)
+
+# tkinter set up
+root = Tk()
+root.title("Option Visualiser - Cryptarbitrage")
+root.iconbitmap('cryptarbitrage_icon_96px.ico')
+root.minsize(600, 400)
+
+# Details input frame
+details_frame = LabelFrame(root, text="Details", padx=2, pady=2)
+details_frame.grid(row=0, column=0, padx=2, pady=2, sticky=NW)
+# Chart frames
+chart1_frame = LabelFrame(root, text="Option Profit/Loss", padx=2, pady=2)
+chart1_frame.grid(row=0, column=1, padx=2, pady=2)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def calculate_profit():
+    premium = float(premium_input.get())
+    strike = float(strike_price_input.get())
+    chart_low = float(chart_minprice_input.get())
+    chart_high = float(chart_maxprice_input.get())
+    if selected_option_type.get() == "Call":
+        option_type = "C"
+    else:
+        option_type = "P"
+    profit_list = oc.option_profit_expiry(premium, strike, chart_low, chart_high, option_type, step_number)
+    return profit_list
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def plot_charts():
+    # Destroy old charts if any
+    for widgets in chart1_frame.winfo_children():
+        widgets.destroy()
+    # Generate x axis values
+    x_range = []
+    chart_low = float(chart_minprice_input.get())
+    chart_high = float(chart_maxprice_input.get())
+    for step in range(0, step_number + 1):
+        x_range.append(step * (chart_high - chart_low) / 100 + chart_low)
+    option1_data = calculate_profit()
+    option1_np = np.array(option1_data)
+    zero_np = np.array([0] * (step_number + 1))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # CHART 1
+    # the figure that will contain the plot
+    fig1 = Figure(figsize=chart_size, dpi=100)
+    # adding the subplot
+    plot1 = fig1.add_subplot(111)
+    # plotting the graph
+    plot1.plot(x_range, option1_data, linewidth=2, label='$' + str(strike_price_input.get()) + ' ' + selected_option_type.get())
+    plot1.fill_between(x_range, option1_data, 0, where=(option1_np < zero_np), facecolor='red', interpolate=True, alpha=0.15)
+    plot1.fill_between(x_range, option1_data, 0, where=(option1_np >= zero_np), facecolor='green', interpolate=True, alpha=0.15)
+    plot1.set_xlabel('Underlying Price')
+    plot1.set_ylabel('Profit/Loss')
+    # plot1.set_title('Chart Title')
+    plot1.legend()
+    plot1.grid(True, alpha=0.25)
+    fig1.tight_layout()
+    # creating the Tkinter canvas
+    # containing the Matplotlib figure
+    canvas1 = FigureCanvasTkAgg(fig1, master=chart1_frame)
+    canvas1.draw()
+    # placing the canvas on the Tkinter window
+    canvas1.get_tk_widget().pack()
+    # creating the Matplotlib toolbar
+    toolbar = NavigationToolbar2Tk(canvas1, chart1_frame)
+    toolbar.update()
+    # placing the toolbar on the Tkinter window
+    canvas1.get_tk_widget().pack()
+
+    plt.show()
+
+
+# details_frame components
+selected_option_type = StringVar()
+selected_option_type.set("Call")
+option_type_label = Label(details_frame, text="Option Type: ")
+option_type_label.grid(row=0, column=0)
+option_type_dropdown = OptionMenu(details_frame, selected_option_type, "Call", "Put")
+option_type_dropdown.grid(row=0, column=1)
+option_type_dropdown.config(width=10)
+
+premium_label = Label(details_frame, text="Option Premium: ")
+premium_label.grid(row=1, column=0)
+premium_input = Entry(details_frame, width=15)
+premium_input.grid(row=1, column=1, padx=5, pady=5)
+
+strike_price_label = Label(details_frame, text="Strike Price: ")
+strike_price_label.grid(row=2, column=0)
+strike_price_input = Entry(details_frame, width=15)
+strike_price_input.grid(row=2, column=1, padx=5, pady=5)
+
+chart_minprice_label = Label(details_frame, text="Chart Min Price: ")
+chart_minprice_label.grid(row=3, column=0)
+chart_minprice_input = Entry(details_frame, width=15)
+chart_minprice_input.grid(row=3, column=1, padx=5, pady=5)
+
+chart_maxprice_label = Label(details_frame, text="Chart Max Price: ")
+chart_maxprice_label.grid(row=4, column=0)
+chart_maxprice_input = Entry(details_frame, width=15)
+chart_maxprice_input.grid(row=4, column=1, padx=5, pady=5)
+
+# button that displays the plot
+plot_button = Button(master=details_frame,
+                     command=plot_charts,
+                     height=2,
+                     width=10,
+                     text="Plot")
+
+plot_button.grid(row=5, column=0, columnspan=2)
+
+root.mainloop()
--- option_calculations.py
+++ option_calculations.py
@@ -4,28 +4,20 @@ from scipy.stats import norm
 N = norm.cdf
 Np = norm.pdf
 
-S = 3000
-K = 2000
-T = 0.5692439751395231
-R = 0
-sigma = 0.9578
-option_type = "P"
 
-
-def option_profit(premium, strike, chart_low, chart_high, option_type="C"):
+def option_profit_expiry(premium, strike, chart_low, chart_high, option_type, step_number):
     profit_list = []
-    steps = 100
-    step = (chart_high - chart_low) / steps
+    step = (chart_high - chart_low) / step_number
     count = 0
     if option_type == "C":
-        while count < steps + 1:
+        while count < step_number + 1:
             if chart_low + (count * step) < strike:
                 profit_list.append(0 - premium)
             else:
                 profit_list.append((chart_low + (count * step)) - strike - premium)
             count = count + 1
     else:
-        while count < steps + 1:
+        while count < step_number + 1:
             if chart_low + (count * step) < strike:
                 profit_list.append(strike - (chart_low + (count * step)) - premium)
             else:
@@ -34,20 +26,19 @@ def option_profit(premium, strike, chart_low, chart_high, option_type="C"):
     return profit_list
 
 
-def inverse_option_profit(premium, strike, chart_low, chart_high, option_type="C"):
+def inverse_option_profit_expiry(premium, strike, chart_low, chart_high, option_type, step_number):
     profit_list = []
-    steps = 100
-    step = (chart_high - chart_low) / steps
+    step = (chart_high - chart_low) / step_number
     count = 0
     if option_type == "C":
-        while count < steps + 1:
+        while count < step_number + 1:
             if chart_low + (count * step) < strike:
                 profit_list.append(0 - premium)
             else:
                 profit_list.append(((chart_low + (count * step)) - strike)/(chart_low + (count * step)) - premium)
             count = count + 1
     else:
-        while count < steps + 1:
+        while count < step_number + 1:
             if chart_low + (count * step) < strike:
                 profit_list.append((strike - (chart_low + (count * step)))/(chart_low + (count * step)) - premium)
             else:
@@ -56,7 +47,7 @@ def inverse_option_profit(premium, strike, chart_low, chart_high, option_type="C
     return profit_list
 
 
-def bs_price(S, K, T, R, sigma, option_type="C"):
+def bs_price(S, K, T, R, sigma, option_type):
     d1 = (np.log(S / K) + (R + sigma ** 2 / 2) * T) / (sigma * np.sqrt(T))
     d2 = d1 - sigma * np.sqrt(T)
 
@@ -67,7 +58,7 @@ def bs_price(S, K, T, R, sigma, option_type="C"):
     return price
 
 
-def bs_delta(S, K, T, R, sigma, option_type="C"):
+def bs_delta(S, K, T, R, sigma, option_type):
     d1 = (np.log(S / K) + (R + sigma ** 2 / 2) * T) / (sigma * np.sqrt(T))
     if option_type == "C":
         delta = N(d1)
@@ -90,7 +81,7 @@ def bs_vega(S, K, T, R, sigma):
     return vega * 0.01
 
 
-def bs_theta(S, K, T, R, sigma, option_type="C"):
+def bs_theta(S, K, T, R, sigma, option_type):
     d1 = (np.log(S / K) + (R + sigma ** 2 / 2) * T) / (sigma * np.sqrt(T))
     d2 = d1 - sigma * np.sqrt(T)
 
@@ -101,7 +92,7 @@ def bs_theta(S, K, T, R, sigma, option_type="C"):
     return theta / 365
 
 
-def bs_rho(S, K, T, R, sigma, option_type="C"):
+def bs_rho(S, K, T, R, sigma, option_type):
     d1 = (np.log(S / K) + (R + sigma ** 2 / 2) * T) / (sigma * np.sqrt(T))
     d2 = d1 - sigma * np.sqrt(T)
 
@@ -110,11 +101,3 @@ def bs_rho(S, K, T, R, sigma, option_type="C"):
     elif option_type == "P":
         rho = -K * T * np.exp(-R * T) * N(-d2)
     return rho * 0.01
-
-
-#print("Price: ", bs_price(S, K, T, R, sigma, option_type="P"))
-#print("Delta: ", bs_delta(S, K, T, R, sigma, option_type))
-#print("Gamma: ", bs_gamma(S, K, T, R, sigma, option_type="P"))
-#print("Vega: ", bs_vega(S, K, T, R, sigma, option_type="P"))
-#print("Theta: ", bs_theta(S, K, T, R, sigma, option_type="P"))
-#print("Rho: ", bs_rho(S, K, T, R, sigma, option_type="P"))
